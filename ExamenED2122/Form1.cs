ed404e7bb31a748250fab7d5a092583922feaa6c
gutierrez21101@iesmarenostrum.com <gutierrez21101@iesmarenostrum.com>
2022-06-06 17:44:01 +0200

    cambio modificado

--- ExamenED2122/Form1.cs
+++ ExamenED2122/Form1.cs
@@ -11,7 +11,7 @@ namespace ExamenED2122
 {
     public partial class Form1 : Form
     {
-        double cantidadTotal;
+        ;
         public Form1()
         {
             InitializeComponent();
@@ -19,20 +19,31 @@ namespace ExamenED2122
 
         private void btCalcular_Click(object sender, EventArgs e)
         {
-            double importe = Convert.ToDouble(txtImporte.Text);
-            double IVA = 0.21;
+           
+            double importeAIGC2122= double.Parse(txtImporte.Text);
+            double ivaAIGC2122 = 0.21;
             if (rbReducido.Checked == true)  // IVA reducido, 10%
-                IVA = 0.1;
+                ivaAIGC2122 = 0.1;
             if (rbSuper.Checked == true)  // IVA super reducido, 4%
-                IVA = 0.4;
-            importe = importe * IVA; // Calculamos el importe con IVA
-            cantidadTotal = importe; // Se lo añadimos a la cantidad total
-            txtResultado.Text = Convert.ToString(importe);
+                ivaAIGC2122 = 0.4;
+            importeAIGC2122 = importeAIGC2122 * ivaAIGC2122; // Calculamos el importe con IVA
+            cantidadTotalAIGC2122 = importeAIGC2122; // Se lo añadimos a la cantidad total
+            txtResultado.Text = Convert.ToString(importeAIGC2122);
         }
 
         private void btTotal_Click(object sender, EventArgs e)
         {
-            txtResultado.Text = Convert.ToString(cantidadTotal);
+            txtResultado.Text = Convert.ToString(cantidadTotalAIGC2122);
         }
-    }
+
+        private void Form1_Load(object sender, EventArgs e)
+            {
+
+            }
+
+        private void label1_Click(object sender, EventArgs e)
+            {
+
+            }
+        }
 }
